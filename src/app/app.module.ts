00bc00a27c97885b0530e40861cda80e9703741d
Shanthamurthy <shanthamurthy.gs@learnship.com>
2020-12-13 15:36:41 +0530

    Skeleton Inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SkeletonComponent } from './components/skeleton/skeleton.component';
+import { NotesFoldersComponent } from './components/notes-folders/notes-folders.component';
+import { NotesListsComponent } from './components/notes-lists/notes-lists.component';
+import { NotesEditorComponent } from './components/notes-editor/notes-editor.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkeletonComponent,
+    NotesFoldersComponent,
+    NotesListsComponent,
+    NotesEditorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/notes-lists/notes-lists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-lists',
+  templateUrl: './notes-lists.component.html',
+  styleUrls: ['./notes-lists.component.css']
+})
+export class NotesListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
